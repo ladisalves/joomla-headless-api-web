a6d6873fb241c1f43f333338fde2f7aaca605e19
ladislaves <ladislaves@gmail.com>
2025-09-25 21:11:01 +0200

    feat: https on local dev

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import fs from 'fs';
+import path from 'path';
+
+const certDir = path.resolve('../certs');
+const certPath = path.join(certDir, 'localhost.pem');
+const keyPath = path.join(certDir, 'localhost-key.pem');
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: {
+      https: {
+        key: fs.readFileSync(keyPath),
+        cert: fs.readFileSync(certPath),
+      },
+      host: 'localhost',
+      port: 4000,
+    },
+  },
+});
